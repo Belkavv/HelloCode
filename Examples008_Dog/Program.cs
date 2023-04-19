52c673792f6551da0f4b2616fe30b41a627f66e1
Belkavv <belkax111@mail.ru>
2023-04-19 10:00:13 +0300

    Examples 008 2 Friend and Dog

---
+++ Examples008_Dog/Program.cs
@@ -0,0 +1,26 @@
+﻿int speedFirstFriend = 1,
+    speedSecondFriend = 3,
+    speedDog = 5;
+
+int distance = 10000,
+    friend = 2,
+    count = 0,
+    time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (speedFirstFriend + speedDog);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (speedSecondFriend + speedDog);
+        friend = 1;
+    }
+    distance = distance - (speedFirstFriend + speedSecondFriend) * time;
+    count++;
+}    
+
+Console.WriteLine(distance);
